0b759187bc7c0c8ee09252c0839211c69f9250bf
Gary Wu <s19003045@gmail.com>
2019-12-15 18:07:23 +0800

    Create users seed file for local test

---
+++ seeders/20191215092356-local-seed-user.js
@@ -0,0 +1,73 @@
+"use strict";
+const faker = require("faker");
+const bcrypt = require("bcrypt-nodejs");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // 建立 root(role:admin), user1(role:user), user2(role:user) 三個 user 種子
+    queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "root",
+          email: "root@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          description: faker.lorem.text().substring(0, 200),
+          role: "admin",
+          country: 'Taiwan',
+          timezone: 'Asia/Taipei',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          username: "user1",
+          email: "user1@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          description: faker.lorem.text().substring(0, 200),
+          role: "user",
+          country: 'Taiwan',
+          timezone: 'Asia/Taipei',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          username: "user2",
+          email: "user2@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          description: faker.lorem.text().substring(0, 200),
+          role: "user",
+          country: 'Taiwan',
+          timezone: 'Asia/Taipei',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    );
+
+    // 建立 5 個 faker user(role:user) 種子
+    return queryInterface.bulkInsert(
+      "Users",
+      Array.from({ length: 5 }).map(d => ({
+        username: faker.name.findName(),
+        email: faker.internet.email(),
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+        avatar: faker.image.imageUrl(),
+        description: faker.lorem.text().substring(0, 200),
+        role: "user",
+        country: 'Taiwan',
+        timezone: 'Asia/Taipei',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
