16449d11ed54ec4004d787dafcbdf88e4b68e839
Sean <siang720@gmail.com>
2019-12-18 08:09:39 +0800

    GET /courses controllers

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,48 @@
 const db = require("../models");
+const Course = db.Course;
 
 const courseController = {
   getCourses: (req, res) => {
-    return res.render('index')
-  }
-}
+    console.log(req.query);
+    // 取得sort功能要依據哪個變數排序所有課程
+    let order = "intoMarketDate";
+    if (req.query.order === "評價由高到低") {
+      order = ["ratingAverage", "DESC"];
+    }
+    if (req.query.order === "學生人數由多到少") {
+      order = ["studentCount", "DESC"];
+    }
+    if (req.query.order === "評價人數由多到少") {
+      order = ["ratingCount", "DESC"];
+    }
+    if (req.query.order === "課程時數由多到少") {
+      order = ["totalTime", "DESC"];
+    }
+    if (req.query.order === "課程時數由少到多") {
+      order = ["totalTime", "ASC"];
+    }
+    if (req.query.order === "價格由高到低") {
+      order = ["price", "DESC"];
+    }
+    if (req.query.order === "價格由低到高") {
+      order = ["price", "ASC"];
+    }
 
-module.exports = courseController
+    Course.findAll({
+      attributes: [
+        "name",
+        "ratingAverage",
+        "studentCount",
+        "totalTime",
+        "price"
+      ],
+      where: { status: "intoMarket" },
+      order: [order]
+    }).then(courses => {
+      return res.render("courses", { courses });
+      // return res.json(courses);
+    });
+  }
+};
+
+module.exports = courseController;
