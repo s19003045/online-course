2d2b4077cab4d86b48a1cf68bfae87da053388b0
Gary Wu <s19003045@gmail.com>
2019-12-20 00:10:59 +0800

    Add comment to column in every model

--- models/coursecategory.js
+++ models/coursecategory.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const CourseCategory = sequelize.define('CourseCategory', {
-    mainCategoName: DataTypes.STRING,
-    subCategoName: DataTypes.STRING
+    mainCategoName: DataTypes.STRING, //主要類別
+    subCategoName: DataTypes.STRING //次要類別
   }, {});
   CourseCategory.associate = function (models) {
     CourseCategory.hasMany(models.Course)
