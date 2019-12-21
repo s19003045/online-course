d746ae95fb3963842cadab0ff7c7661a839cdc83
Gary Wu <s19003045@gmail.com>
2019-12-21 12:23:58 +0800

    Modify CourseReviewPost & CourseReviewReply seed for local test

--- seeders/20191220121321-local-seed-coursereviewpost.js
+++ seeders/20191220121321-local-seed-coursereviewpost.js
@@ -1,6 +1,16 @@
 "use strict";
 const faker = require("faker");
 const bcrypt = require("bcrypt-nodejs");
+const postCreatedAt = []
+const postUpdatedAt = []
+
+let i
+for (i = 0; i < 50; i++) {
+  postCreatedAt.push(faker.date.past())
+}
+for (i = 0; i < 50; i++) {
+  postUpdatedAt.push(faker.date.future())
+}
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -12,20 +22,20 @@ module.exports = {
         message: faker.lorem.text().substring(0, 200),
         CourseId: Math.floor(Math.random() * 50) + 1,
         UserId: Math.floor(Math.random() * 8) + 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
+        createdAt: postCreatedAt[Math.floor(Math.random() * postCreatedAt.length)],
+        updatedAt: postUpdatedAt[Math.floor(Math.random() * postUpdatedAt.length)]
       }))
       ,
       {}
     )
     return queryInterface.bulkInsert(
       "courseReviewReplies",
-      Array.from({ length: 400 }).map(d => ({
+      Array.from({ length: 1000 }).map(d => ({
         message: faker.lorem.text().substring(0, 200),
         CourseReviewPostId: Math.floor(Math.random() * 200),
         UserId: Math.floor(Math.random() * 8) + 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
+        createdAt: postCreatedAt[Math.floor(Math.random() * postCreatedAt.length)],
+        updatedAt: postUpdatedAt[Math.floor(Math.random() * postUpdatedAt.length)]
       }))
       ,
       {}
