a1141d53d84daf0fb79769224980039781a3a912
Sean <siang720@gmail.com>
2019-12-23 00:44:58 +0800

    Add seeders for discuss-post and discuss-reply

---
+++ seeders/20191222154155-local-seed-faker-discuss-post.js
@@ -0,0 +1,27 @@
+"use strict";
+const faker = require("faker");
+
+// 為每一門課程產生discussion posts
+let posts = [];
+for (let i = 51; i < 101; i++) {
+  for (let j = 2; j < 7; j++) {
+    posts.push({
+      subject: faker.lorem.sentence(),
+      message: faker.lorem.sentence(),
+      UserId: j,
+      CourseId: i,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
+  }
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("DiscussPosts", posts);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("DiscussPosts", null, {});
+  }
+};
