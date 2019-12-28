049c13ca3d8fa8d96cbf77da6d56d2984d4a3998
Ariel Huang <ariel92911@gmail.com>
2019-12-28 22:28:28 +0800

    Add getUser feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,6 +53,15 @@ const userController = {
     req.logout();
     res.redirect("/signin");
   },
+
+  //使用者可以看個人帳號資訊
+  getUser: (req, res) => {
+    User.findByPk(req.params.id)
+      .then(user => {
+        return res.render('user', { user })
+      })
+  },
+
   // 開課者可以瀏灠自己開的課及相關資訊
   getTeachCourses: (req, res) => {
     Course.findAll({
