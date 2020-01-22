544d1c7828bbd08120b2b53e21f6115990d2b5f1
Sean <siang720@gmail.com>
2020-01-22 12:43:46 +0800

    if no course then redirect to homepage

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -113,19 +113,18 @@ const courseController = {
           courses: data,
           order: req.query.order,
           route: "all",
-          reqUrl: req.url,
+          reqUrl: req.url
         });
       } else {
-        User.findByPk(req.user.id)
-          .then(user => {
-            return res.render("courses", {
-              courses: data,
-              order: req.query.order,
-              route: "all",
-              reqUrl: req.url,
-              user
-            });
-          })
+        User.findByPk(req.user.id).then(user => {
+          return res.render("courses", {
+            courses: data,
+            order: req.query.order,
+            route: "all",
+            reqUrl: req.url,
+            user
+          });
+        });
       }
     });
   },
@@ -147,10 +146,7 @@ const courseController = {
     }).then(category => {
       // 找不到類別
       if (!category) {
-        req.flash(
-          "error_messages",
-          "目前還沒有該類別的課程，本站將陸續新增，不好意思！"
-        );
+        req.flash("error_messages", "目前尚未規劃該類別，本站將陸續新增！");
         res.redirect("/courses");
       } else {
         Course.findAll({
@@ -170,21 +166,25 @@ const courseController = {
         }).then(courses => {
           // 該類別沒有任何課程
           if (courses.length === 0) {
-            let no_courses = true;
-            return res.render("courses", { no_courses });
+            req.flash(
+              "error_messages",
+              "目前該類別還沒有任何課程，本站將陸續新增！"
+            );
+            res.redirect("/courses");
+          } else {
+            // 辨認課程是否被登入的使用者購買
+            const data = courses.map(c => ({
+              ...c.dataValues,
+              enrolled: userEnrolledId.includes(c.dataValues.id)
+            }));
+            return res.render("courses", {
+              courses: data,
+              order: req.query.order,
+              route: "mainCate",
+              mainCategoName: req.params.mainCategoName,
+              reqUrl: req.url
+            });
           }
-          // 辨認課程是否被登入的使用者購買
-          const data = courses.map(c => ({
-            ...c.dataValues,
-            enrolled: userEnrolledId.includes(c.dataValues.id)
-          }));
-          return res.render("courses", {
-            courses: data,
-            order: req.query.order,
-            route: "mainCate",
-            mainCategoName: req.params.mainCategoName,
-            reqUrl: req.url
-          });
         });
       }
     });
@@ -207,10 +207,7 @@ const courseController = {
     }).then(category => {
       // 找不到類別
       if (!category) {
-        req.flash(
-          "error_messages",
-          "目前還沒有該類別的課程，本站將陸續新增，不好意思！"
-        );
+        req.flash("error_messages", "目前尚未規劃該類別，本站將陸續新增！");
         res.redirect("/courses");
       } else {
         CourseSubCategory.findOne({
@@ -245,22 +242,26 @@ const courseController = {
             }).then(courses => {
               // 該類別沒有任何課程
               if (courses.length === 0) {
-                let no_courses = true;
-                return res.render("courses", { no_courses });
+                req.flash(
+                  "error_messages",
+                  "目前該類別還沒有任何課程，本站將陸續新增！"
+                );
+                res.redirect("/courses");
+              } else {
+                // 辨認課程是否被登入的使用者購買
+                const data = courses.map(c => ({
+                  ...c.dataValues,
+                  enrolled: userEnrolledId.includes(c.dataValues.id)
+                }));
+                return res.render("courses", {
+                  courses: data,
+                  order: req.query.order,
+                  route: "subCate",
+                  mainCategoName: req.params.mainCategoName,
+                  subCategoName: req.params.subCategoName,
+                  reqUrl: req.url
+                });
               }
-              // 辨認課程是否被登入的使用者購買
-              const data = courses.map(c => ({
-                ...c.dataValues,
-                enrolled: userEnrolledId.includes(c.dataValues.id)
-              }));
-              return res.render("courses", {
-                courses: data,
-                order: req.query.order,
-                route: "subCate",
-                mainCategoName: req.params.mainCategoName,
-                subCategoName: req.params.subCategoName,
-                reqUrl: req.url
-              });
             });
           }
         });
